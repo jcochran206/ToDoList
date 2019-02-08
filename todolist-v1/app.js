492307628469b40f4a381398867239a7f63df19a
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-07 23:31:22 -0800

    added loop to iterate thru new items in array

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,40 +6,57 @@ const app = express();
 
 const port = 3000;
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", function(req, res){
 
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-}
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay : day});
+
+//   switch (currentDay) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+
+  res.render("list", {kindOfDay : day, newListItems: items});
 });
 
+app.post("/", function(req, res){
+  var item = req.body.newItem;
 
+  items.push(item);
+
+  res.redirect("/");
+});
 
 app.listen(port, () => console.log(`application is running on ${port}`));
