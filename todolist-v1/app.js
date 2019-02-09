96fd83b5968870a2513e5de93bf6fd21e6d49c91
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-08 22:57:26 -0800

    added stylesheet to project using public folder

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -10,9 +10,12 @@ var items = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDay();
@@ -25,33 +28,36 @@ app.get("/", function(req, res){
   var day = today.toLocaleDateString("en-US", options);
 
 
-//   switch (currentDay) {
-//   case 0:
-//     day = "Sunday";
-//     break;
-//   case 1:
-//     day = "Monday";
-//     break;
-//   case 2:
-//      day = "Tuesday";
-//     break;
-//   case 3:
-//     day = "Wednesday";
-//     break;
-//   case 4:
-//     day = "Thursday";
-//     break;
-//   case 5:
-//     day = "Friday";
-//     break;
-//   case 6:
-//     day = "Saturday";
-// }
+  //   switch (currentDay) {
+  //   case 0:
+  //     day = "Sunday";
+  //     break;
+  //   case 1:
+  //     day = "Monday";
+  //     break;
+  //   case 2:
+  //      day = "Tuesday";
+  //     break;
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //   case 6:
+  //     day = "Saturday";
+  // }
 
-  res.render("list", {kindOfDay : day, newListItems: items});
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   var item = req.body.newItem;
 
   items.push(item);
