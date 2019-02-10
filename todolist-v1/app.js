87a9e37d8eaa57257ccc6f5810f6d1651978238e
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-09 17:02:36 -0800

    added css template features in order to make layouts easier and last used created module to pass data into app

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,12 +1,14 @@
 // jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 const port = 3000;
 
-var items = [];
+let items = ["books", "cooking"];
+let workItems = ["new work Items"];
 
 app.set('view engine', 'ejs');
 
@@ -17,52 +19,38 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-
-
-  //   switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-  //   case 2:
-  //      day = "Tuesday";
-  //     break;
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-  //   case 6:
-  //     day = "Saturday";
-  // }
+let day = date.getDay();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 });
 
 app.post("/", function(req, res) {
-  var item = req.body.newItem;
+  console.log(req.body.list);
+  let item = req.body.newItem;
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-  items.push(item);
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
-  res.redirect("/");
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
 });
 
 app.listen(port, () => console.log(`application is running on ${port}`));
