558150763b7592cbf0415567215bcf11c45e63c7
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-25 10:52:16 -0800

    added statement to add items into database and redirect back to homepage to show additions

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -37,33 +37,35 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if(err){
-    console.log(err)
-  }else{
-    console.log("Successfully added items");
-  }
-});
 
 app.get("/", function(req, res) {
 
-//const day = date.getDate(); listTitle: day review in verison one
-
-  res.render("list", {listTitle: Today, newListItems: items});
-
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err)
+        }else{
+          console.log("Successfully added items");
+        }
+      });
+      res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  });
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+  res.redirect('/');
 });
 
 app.get("/work", function(req,res){
