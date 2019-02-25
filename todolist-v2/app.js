bcfc6293ea011180801da2d24cc169f7e3e73087
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-25 11:31:18 -0800

    modified css to exclude form, added delect function in post to delete items from db, and updated views js to allow for delete function on click from checkbox

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -68,6 +68,19 @@ app.post("/", function(req, res){
   res.redirect('/');
 });
 
+app.post('/delete', function(req, res){
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("successfully deleted from database");
+      res.redirect('/');
+    }
+  });
+});
+
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
