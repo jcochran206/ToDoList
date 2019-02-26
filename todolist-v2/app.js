f769f4d4b8892cc5e29c8da7e329a18a799ca11e
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-02-25 17:09:33 -0800

    added customListName and routes to coorespond to the list and add items to custom lists

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -37,6 +37,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 
 app.get("/", function(req, res) {
 
@@ -56,34 +63,62 @@ app.get("/", function(req, res) {
   });
 });
 
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if(!err){
+      if(!foundList){
+        //create new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        // show list
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+});
+
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName
   });
 
-  item.save();
-  res.redirect('/');
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
+  }else{
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+
 });
 
 app.post('/delete', function(req, res){
   const checkedItemId = req.body.checkbox;
 
   Item.findByIdAndRemove(checkedItemId, function(err){
-    if(err){
-      console.log(err);
-    }else{
+    if(!err){
       console.log("successfully deleted from database");
-      res.redirect('/');
+      res.redirect("/");
     }
   });
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+
 
 app.get("/about", function(req, res){
   res.render("about");
